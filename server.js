ff4ea85a1cd8535c02232f4b8ed015295e9fd873
jellemdijkstra-lab <jelle.m.dijkstra@gmail.com>
2026-04-08 22:33:01 -0700

    changed layout

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const ROOT = __dirname;
+
+const mime = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif':  'image/gif',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+  '.json': 'application/json',
+  '.woff': 'font/woff',
+  '.woff2':'font/woff2',
+};
+
+http.createServer((req, res) => {
+  const urlPath = req.url === '/' ? '/index.html' : req.url.split('?')[0];
+  const filePath = path.join(ROOT, urlPath);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': mime[ext] || 'application/octet-stream' });
+      res.end(data);
+    }
+  });
+}).listen(PORT, () => console.log(`Serving at http://localhost:${PORT}`));
